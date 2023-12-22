f6f21008156d43f3e3363fa532bbbeb2c41b4af5
bhargav <karandave503@gmail.com>
2023-12-22 15:57:18 +0530

    change look

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,91 +1,129 @@
 import "../assets/Style/home.scss";
-import homepage from "../../public/homepage1.jpg";
-import homepage2 from "../../public/homepage2.jpg";
-import homepage3 from "../../public/homepage3.webp";
-import gif from '../../public/icons/gif3.gif';
-import gpt from '../../public/icons/chatgpt.png';
-import gmail from '../../public/icons/Gmail-logo.png'
-import linkdin from '../../public/icons/linkedin.png'
-import movie from '../../public/icons/popcorn cup.webp'
-import whatsapp from '../../public/icons/whatsapp.webp'
-import youtube from '../../public/icons/youtube.png'
-import vercel from '../../public/icons/vercel.png'
-import git from '../../public/icons/github.png'
-import git2 from '../../public/icons/git2.0.png'
-import google from '../../public/icons/google.png'
 
+import homepage from "../../public/homepage7.jpg";
+import gif from "../../public/jarvis2.gif";
+import gpt from "../../public/icons/chatgpt.png";
+import gmail from "../../public/icons/Gmail-logo.png";
+import linkdin from "../../public/icons/linkedin.png";
+import movie from "../../public/icons/popcorn cup.webp";
+import whatsapp from "../../public/icons/whatsapp.webp";
+import youtube from "../../public/icons/youtube.png";
+import vercel from "../../public/icons/vercel.png";
+import git2 from "../../public/icons/git2.0.png";
+import google from "../../public/icons/google.png";
+import chess from "../../public/icons/chess.png";
 
+import figma from "../../public/icons/figma.png";
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
 
-import figma from '../../public/icons/figma.png'
-import { useState } from "react";
-const date = new Date();
-const hour = date.getHours(0 - 24);
-
- 
 const Home = () => {
-if(hour > 12){
-    hour - 12;
-}else{
-    hour;
-}
  
-const afternoon = "Good Afternoon";
-const evening = "Good Evening";
-const night = "Good Night"
+
+  let time = new Date().toLocaleTimeString();
+  let time2 = new Date();
+
+  const hour = time2.getHours();
+  const [clocknw, setclock] = useState(time);
+
+  const updateTime = () => {
+  let time =  new Date().toLocaleTimeString(); 
+   setclock(time);
+  };
 
 
-
-
+  setInterval(updateTime,1000)
  
-  const { time, settime } = useState("");
-
  
-  
-   
-  return (
-    <div className="home">
-      <img src={homepage3} alt="" />
 
-      <h1>
-        Hello Karan, <br /> 
-      </h1>
+  const Clock = () => {
+    let newhour;
 
-      <h2>
-   how  are you
-      </h2>
+    // if (hour > 12) {
+    //   newhour = hour - 12;
+    // }
 
-<div className="gif">
-
-     <img src={gif} alt="" />
-</div>
-
-<div className="icons">
-  <div className="container">
-<Item src={figma} link={'https://www.figma.com/files/drafts?fuid=1202972142199126152'}/>
-<Item link={'https://chat.openai.com/'} src={gpt}/>
-<Item src={youtube} link={'https://www.youtube.com/'}/>
-<Item src={whatsapp} link={'https://web.whatsapp.com/'}/>
-<Item src={movie} link={'https://moviesmod.pro/'}/>
-<Item src={linkdin} link={'https://www.linkedin.com/in/karan-dave-18702324b/'}/>
-<Item src={gmail} link={'https://mail.google.com/mail/u/0/#inbox'}/>
-<Item src={git2} link={'https://github.com/'}/>
-<Item src={vercel} link={'https://vercel.com/dashboard'}/>
-<Item src={google} link={'https://www.google.com/'}/>
-
-  </div>
-</div>
+    return (
+      <div className="clock-container">
+ <h1 className="clock=font">{clocknw}</h1>
       </div>
     );
+  };
+
+  const [greeting, setgreeting] = useState("");
+
+  useEffect(() => {
+    if (hour < 12) {
+      setgreeting("Good Morning");
+    } else if (hour >= 12 && hour < 16) {
+      setgreeting("Good Afternoon");
+    } else if (hour > 16 && hour < 19) {
+      setgreeting("Good Evening");
+    } else if (hour > 19 && hour < 24) {
+      setgreeting("Good Night");
+    }
+  });
+
+  return (
+    <div className="home">
+      <img src={homepage} alt="homepage" />
+
+      <h1>
+        Hello Karan, <br />
+      </h1>
+
+      <h2>{greeting}</h2>
+
+      <div className="gif">
+        <img
+          src={gif}
+          alt="gif-imageloading"
+          // onLoad={() => setImageLoading(false)}
+        />
+      </div>
+
+      <div className="clock">
+        <Clock />
+      </div>
+
+      <div className="icons">
+        <div className="container">
+          <Item
+            src={figma}
+            link={"https://www.figma.com/files/drafts?fuid=1202972142199126152"}
+          />
+          <Item link={"https://chat.openai.com/"} src={gpt} />
+          <Item src={youtube} link={"https://www.youtube.com/"} />
+          <Item src={whatsapp} link={"https://web.whatsapp.com/"} />
+          <Item src={movie} link={"https://moviesmod.pro/"} />
+          <Item
+            src={linkdin}
+            link={"https://www.linkedin.com/in/karan-dave-18702324b/"}
+          />
+          <Item
+            src={chess}
+            link={
+              "https://www.chess.com/play/online/new?action=createLiveChallenge&base=600&timeIncrement=0"
+            }
+          />
+          <Item src={gmail} link={"https://mail.google.com/mail/u/0/#inbox"} />
+          <Item src={git2} link={"https://github.com/"} />
+          <Item src={vercel} link={"https://vercel.com/dashboard"} />
+          <Item src={google} link={"https://www.google.com/"} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const Item = ({src,link}) =>{
+const Item = ({ src, link }) => {
   return (
     <div className="item">
-      <a href={link} target={'_blank'}>
+      <a href={link} target={"_blank"}>
         <img src={src} alt="" />
       </a>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+    return (
+      <div className="flex space-x-2 justify-center items-center ">
+        <span className="sr-only">Loading...</span>
+        <div className="h-8 w-8 bg-[#000] rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="h-8 w-8 bg-[#000]  rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="h-8 w-8 bg-[#000] rounded-full animate-bounce"></div>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
